d19e620af408cc3c8a96559e3ec774f9db0715c9
John Zdanis <zdanis.john@gmail.com>
2015-04-18 21:28:44 -0700

    added simple do loop for properties in index

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,64 @@
 class PropertiesController < ApplicationController
+   before_action :set_property, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def edit
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    respond_to do |format|
+      if @property.save
+        format.html { redirect_to @property, notice: 'Property was successfully created.' }
+        format.json { render :show, status: :created, location: @property }
+      else
+        format.html { render :new }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+    respond_to do |format|
+      if @property.update(property_params)
+        format.html { redirect_to @property, notice: 'Property was successfully updated.' }
+        format.json { render :show, status: :ok, location: @property }
+      else
+        format.html { render :edit }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @property.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'Property was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_property
+      @property = Property.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def property_params
+      params[:property]
+    end
 end
